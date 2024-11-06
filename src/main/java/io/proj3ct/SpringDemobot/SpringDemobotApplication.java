b2ddcb54c4b5eff3a79ba75ebf414f526d54d1cc
sp0kery <zhukovdeniszh@gmail.com>
2024-11-06 15:33:49 +0300

    Initial commit

---
+++ src/main/java/io/proj3ct/SpringDemobot/SpringDemobotApplication.java
@@ -0,0 +1,13 @@
+package io.proj3ct.SpringDemobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemobotApplication.class, args);
+	}
+
+}
